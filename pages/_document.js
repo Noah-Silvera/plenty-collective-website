245c6cbb66922199f9912fbc59ecf800354bb49a
Noah Silvera <noah.a.silvera@gmail.com>
2023-05-02 13:23:00 -0700

    Basic navbar styling

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,18 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Image from 'next/image'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <nav>
-          <a href="/">Home</a>
-          <a href="/contact">Contact Us</a>
+        <nav className="flex flex-row bg-purple-300 bg-opacity-30 p-2">
+          <div className="grow">
+            <a href="/" className="w-32 block"><Image src="/logo.svg" height={592} width={1839} /></a>
+          </div>
+          <a href="/contact" className="my-auto">Contact Us</a>
         </nav>
       </Head>
-      <body>
+      <body className="bg-rose-50">
         <Main />
         <NextScript />
       </body>
