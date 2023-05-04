636bf62a380138cfa22d089f7d8e110e51a02aa6
Noah Silvera <noah.a.silvera@gmail.com>
2023-05-03 20:10:47 -0700

    fix compilation bugs

--- components/Page.jsx
+++ components/Page.jsx
@@ -14,11 +14,18 @@ export default function Page({ content }){
       {content.displayTitle && <h1 className="text-4xl pb-8 pt-4">{content.title}</h1>}
       {content.sections.map((section, idx) => {
         return (
-          <div className={classNames({
-            "bg-plenty-pink-300": idx % 2 == 0,
-            "bg-plenty-purple-200": idx % 2 == 1,
-            "bg-opacity-50": true
-          })}>
+          <div key={idx} className={classNames(
+            "max-w-2xl",
+            "mx-auto",
+            "py-4",
+            "px-12",
+            "m-4",
+            "bg-opacity-50",
+            {
+              "bg-plenty-pink-300": idx % 2 == 0,
+              "bg-plenty-purple-200": idx % 2 == 1
+            })
+          }>
             {createElement(eval(sectionComponentLookup[section.type]), {content: section})}
           </div>
         )
