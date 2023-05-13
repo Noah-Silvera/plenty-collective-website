8c76b687ba9270313ffe2f035c68a7426bd4b072
Noah Silvera <noah.a.silvera@gmail.com>
2023-05-13 11:03:45 -0700

    Fix problems with script tags

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,9 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <Script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/js/all.min.js" integrity="sha512-fD9DI5bZwQxOi7MhYWnnNPlvXdp/2Pj3XSTRrFs5FQa4mizyGLnJcN6tuvUS6LbmgN1ut+XGSABKvjN0H6Aoow==" crossOrigin="anonymous" referrerPolicy="no-referrer"></Script>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
       </Head>
       <body className="min-h-screen bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-plenty-pink-300 via-plenty-purple-300 to-plenty-blue-400">
         <Main />
