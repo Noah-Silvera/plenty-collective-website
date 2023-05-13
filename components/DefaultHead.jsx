6b98b1c05c22f9a260d8d26aad15389201239013
Noah Silvera <noah.a.silvera@gmail.com>
2023-05-13 11:06:43 -0700

    Move scripts to right head

--- components/DefaultHead.jsx
+++ components/DefaultHead.jsx
@@ -1,5 +1,8 @@
 import Head from 'next/head'
 import Nav from '@/components/Nav'
+import Link from 'next/link';
+import Script from 'next/script'
+
 
 export default function DefaultHead({ title, pages }) {
   let pageTitle = [title, "Plenty Collective"].filter((val) => !!val).join(" - ")
@@ -8,6 +11,8 @@ export default function DefaultHead({ title, pages }) {
     <title>{pageTitle}</title>
     {/* <link rel="icon" href="/favicon.ico" /> */}
     <Nav pages={pages}></Nav>
+    <Script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/js/all.min.js" integrity="sha512-fD9DI5bZwQxOi7MhYWnnNPlvXdp/2Pj3XSTRrFs5FQa4mizyGLnJcN6tuvUS6LbmgN1ut+XGSABKvjN0H6Aoow==" crossOrigin="anonymous" referrerPolicy="no-referrer"></Script>
+    <Link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
   </Head>
   )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,10 @@ import '@/styles/globals.css'
 import '@/styles/custom-fonts.css'
 import '@splidejs/react-splide/css';
 import DefaultHead from '@/components/DefaultHead'
-import Link from 'next/link';
-import Script from 'next/script'
 
 export default function App({ Component, pageProps }) {
   return <>
     <DefaultHead title={pageProps.pageJSON?.title || null} pages={pageProps.pages}/>
     <Component {...pageProps} />
-    <Script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/js/all.min.js" integrity="sha512-fD9DI5bZwQxOi7MhYWnnNPlvXdp/2Pj3XSTRrFs5FQa4mizyGLnJcN6tuvUS6LbmgN1ut+XGSABKvjN0H6Aoow==" crossOrigin="anonymous" referrerPolicy="no-referrer"></Script>
-    <Link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
   </>
 }
