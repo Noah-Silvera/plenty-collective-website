0c026c23437a8915a2f0364e1459927c299d5e54
Noah Silvera <noah.a.silvera@gmail.com>
2023-05-12 18:10:11 -0700

    Fix unescaped characters

--- components/sections/Quote.jsx
+++ components/sections/Quote.jsx
@@ -1,6 +1,6 @@
 export default function Quote({ content }){
   return <section className="p-4">
-    <h2 className="text-4xl font-thin">"{content.text}"</h2>
+    <h2 className="text-4xl font-thin">&quot;{content.text}&quot;</h2>
     <p className="text-2xl pt-4 pl-[10%] font-regular font-liberation-sans">- {content.author}</p>
   </section>
 }
