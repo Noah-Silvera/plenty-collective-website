4447df0d5fa7c0246123e18b44cafa5c15727906
Noah Silvera <noah.a.silvera@gmail.com>
2023-05-13 11:38:19 -0700

    Fix head problem

--- components/DefaultHead.jsx
+++ components/DefaultHead.jsx
@@ -1,13 +1,11 @@
 import Head from 'next/head'
-import Nav from '@/components/Nav'
 
 export default function DefaultHead({ title, pages }) {
   let pageTitle = [title, "Plenty Collective"].filter((val) => !!val).join(" - ")
   return (
-  <Head>
-    <title>{pageTitle}</title>
-    {/* <link rel="icon" href="/favicon.ico" /> */}
-    <Nav pages={pages}></Nav>
-  </Head>
+    <Head>
+      <title>{pageTitle}</title>
+      {/* <link rel="icon" href="/favicon.ico" /> */}
+    </Head>
   )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -4,10 +4,12 @@ import '@splidejs/react-splide/css';
 import DefaultHead from '@/components/DefaultHead'
 import Link from 'next/link';
 import Script from 'next/script'
+import Nav from '@/components/Nav'
 
 export default function App({ Component, pageProps }) {
   return <>
     <DefaultHead title={pageProps.pageJSON?.title || null} pages={pageProps.pages}/>
+    <Nav pages={pages}></Nav>
     <Component {...pageProps} />
     <Script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/js/all.min.js" integrity="sha512-fD9DI5bZwQxOi7MhYWnnNPlvXdp/2Pj3XSTRrFs5FQa4mizyGLnJcN6tuvUS6LbmgN1ut+XGSABKvjN0H6Aoow==" crossOrigin="anonymous" referrerPolicy="no-referrer"/>
     <Link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
