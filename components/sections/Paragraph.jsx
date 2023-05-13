14ea5b5bd16e6fc1bf611b63dc0edf979187d961
Noah Silvera <noah.a.silvera@gmail.com>
2023-05-13 09:52:01 -0700

    Improve mobile text sizes

--- components/sections/Paragraph.jsx
+++ components/sections/Paragraph.jsx
@@ -5,7 +5,7 @@ export default function Paragraph({ content, idx }){
   return <BoxShadowWrapper idx={idx}>
     <section className="p-4">
       <h2 className="text-4xl text-center">{content.header}</h2>
-      <div className="text-2xl leading-10 tracking-wide font-libre-baskerville">
+      <div className="text-xl sm:text-2xl leading-10 tracking-wide font-libre-baskerville">
         {documentToReactComponents(content.document)}
       </div>
     </section>
