64301fa9f6ec19d0329d715e03092ab68d476b37
Noah Silvera <noah.a.silvera@gmail.com>
2023-05-12 17:49:38 -0700

    Add pretty cloud backgrounds

--- components/Page.jsx
+++ components/Page.jsx
@@ -10,25 +10,43 @@ export default function Page({ content }){
   }
 
   return (
-    <main className="pt-14">
+    <main>
       {content.displayTitle && <h1 className="text-4xl pb-8 pt-4">{content.title}</h1>}
       {content.sections.map((section, idx) => {
         return (
-          <div key={idx} className={classNames(
-            "max-w-2xl",
-            "mx-auto",
-            "py-4",
-            "px-12",
-            "m-4",
-            "bg-opacity-50",
-            "rounded-md",
-            "drop-shadow-lg",
+          <div className={classNames(
+            "bg-cover",
+            "bg-no-repeat",
+            "bg-center",
+            "h-96",
+            "flex",
+            "justify-center",
+            "items-center",
             {
-              "bg-plenty-pink-300": idx % 2 == 0,
-              "bg-plenty-purple-200": idx % 2 == 1
-            })
-          }>
-            {createElement(eval(sectionComponentLookup[section.type]), {content: section})}
+              "bg-[url('/images/cloud-1.jpg')]": idx % 8 == 0,
+              "bg-[url('/images/cloud-2.jpg')]": idx % 8 == 2,
+              "bg-[url('/images/cloud-3.jpg')]": idx % 8 == 4,
+              "bg-[url('/images/cloud-4.jpg')]": idx % 8 == 6,
+            }
+          )}>
+            <div key={idx} className={classNames(
+              "w-[52rem]",
+              "max-w-[90%]",
+              "h-fit",
+              "mx-auto",
+              "py-4",
+              "px-12",
+              "m-4",
+              "bg-opacity-75",
+              "rounded-md",
+              "drop-shadow-lg",
+              {
+                "bg-plenty-pink-300": idx % 2 == 0,
+                "bg-plenty-purple-200": idx % 2 == 1
+              })
+            }>
+              {createElement(eval(sectionComponentLookup[section.type]), {content: section})}
+            </div>
           </div>
         )
       })}
