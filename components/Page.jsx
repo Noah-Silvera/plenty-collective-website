33d0466792af9ba8daeaaec8f157f6a70348815d
Noah Silvera <noah.a.silvera@gmail.com>
2023-05-12 18:07:52 -0700

    Make cloud rendering less jank

--- components/Page.jsx
+++ components/Page.jsx
@@ -9,27 +9,36 @@ export default function Page({ content }){
     "QuoteContent": Quote
   }
 
+  const cloudImageUrls = [
+    '/images/cloud-1.jpg',
+    '/images/cloud-2.jpg',
+    '/images/cloud-3.jpg',
+    '/images/cloud-4.jpg',
+  ]
+
+  const startingCloudImageIndex = 3;
+
   return (
     <main>
       {content.displayTitle && <h1 className="text-4xl pb-8 pt-4">{content.title}</h1>}
       {content.sections.map((section, idx) => {
+        let evenIndex = idx % 2 == 0;
+        let oddIndex = idx % 2 == 1;
+
         return (
-          <div className={classNames(
-            "bg-cover",
-            "bg-no-repeat",
-            "bg-center",
-            "h-96",
-            "flex",
-            "justify-center",
-            "items-center",
-            {
-              "bg-[url('/images/cloud-1.jpg')]": idx % 8 == 0,
-              "bg-[url('/images/cloud-2.jpg')]": idx % 8 == 2,
-              "bg-[url('/images/cloud-3.jpg')]": idx % 8 == 4,
-              "bg-[url('/images/cloud-4.jpg')]": idx % 8 == 6,
-            }
-          )}>
-            <div key={idx} className={classNames(
+          <div key={idx}
+            className={classNames(
+              "bg-cover",
+              "bg-no-repeat",
+              "bg-center",
+              "h-96",
+              "flex",
+              "justify-center",
+              "items-center"
+            )}
+            style={{backgroundImage: evenIndex ? `url(${cloudImageUrls[(idx/2) % cloudImageUrls.length + startingCloudImageIndex]})`: 'none'}}
+          >
+            <div className={classNames(
               "w-[52rem]",
               "max-w-[90%]",
               "h-fit",
@@ -40,10 +49,10 @@ export default function Page({ content }){
               "rounded-md",
               "drop-shadow-xl",
               {
-                "bg-plenty-pink-300": idx % 2 == 0,
-                "bg-plenty-purple-200": idx % 2 == 1,
-                "bg-opacity-75": idx % 2 == 0,
-                "bg-opacity-50": idx % 2 == 1
+                "bg-plenty-pink-300": evenIndex,
+                "bg-plenty-purple-200": oddIndex,
+                "bg-opacity-75": evenIndex,
+                "bg-opacity-50": oddIndex
               })
             }>
               {createElement(eval(sectionComponentLookup[section.type]), {content: section})}
