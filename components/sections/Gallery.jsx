26307f9daf9bf35bdcaccfd3c79c34fe57ffb993
Noah Silvera <noah.a.silvera@gmail.com>
2023-05-13 09:48:16 -0700

    Add splide slides to gallery

--- components/sections/Gallery.jsx
+++ components/sections/Gallery.jsx
@@ -1,19 +1,45 @@
-import Image from "next/image"
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+
+function GallerySlides({ images }) {
+  return images.map((image, idx) => {
+    return (
+      <SplideSlide key={idx}>
+        <img
+          src={image.url}
+          width={image.width}
+          height={image.height}
+          alt={image.description}
+          className="max-w-full sm:max-w-[24rem] mx-auto"
+          ></img>
+      </SplideSlide>
+    )
+  })
+}
 
 export default function Gallery({ content }){
-  return <section className="max-w-full flex flex-wrap justify-center gap-8 max-w-7xl px-8 py-8">
-    {content.images.map((image, idx) => {
-      return (
-        <div key={idx}>
-          <img
-            src={image.url}
-            width={image.width}
-            height={image.height}
-            alt={image.description}
-            className="max-w-full sm:max-w-[24rem]"
-          ></img>
-        </div>
-      )
-    })}
+
+  const defaultSplideOptions = {
+    gap: '2rem'
+  }
+
+  return <section className="max-w-full max-w-7xl px-8 py-8">
+    <Splide className="hidden xl:block" options={{
+      ...defaultSplideOptions,
+      perPage: 3,
+    }}>
+      <GallerySlides images={content.images}></GallerySlides>
+    </Splide>
+    <Splide className="hidden md:block xl:hidden"  options={{
+      ...defaultSplideOptions,
+      perPage: 2,
+    }}>
+      <GallerySlides images={content.images}></GallerySlides>
+    </Splide>
+    <Splide className="md:hidden"  options={{
+      ...defaultSplideOptions,
+      perPage: 1,
+    }}>
+      <GallerySlides images={content.images}></GallerySlides>
+    </Splide>
   </section>
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '@/styles/globals.css'
 import '@/styles/custom-fonts.css'
+import '@splidejs/react-splide/css';
 import DefaultHead from '@/components/DefaultHead'
 
 export default function App({ Component, pageProps }) {
