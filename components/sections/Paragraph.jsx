624d93e9c39a516de2e798c764f31f8e2b1c1c3a
Noah Silvera <noah.a.silvera@gmail.com>
2023-05-29 08:42:18 -0700

    Basic display of images on right left or top (mobile) in a paragraph

--- components/sections/Paragraph.jsx
+++ components/sections/Paragraph.jsx
@@ -1,15 +1,31 @@
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 import BoxShadowWrapper from './BoxShadowWrapper'
-import Image from 'next/image'
+import classNames from 'classnames'
 
 export default function Paragraph({ content, idx }){
   return <BoxShadowWrapper idx={idx}>
-    <section className="p-4">
-      <h2 className="text-4xl text-center">{content.header}</h2>
-      <div className="text-xl sm:text-2xl leading-10 tracking-wide font-libre-baskerville">
-        {documentToReactComponents(content.document)}
+    <section className={classNames(
+      "p-4",
+      "flex",
+      "flex-col",
+      {
+        "md:flex-row": !content.displayImageOnRightSide,
+        "md:flex-row-reverse": content.displayImageOnRightSide
+      }
+    )}>
+       {content.image && (
+        <img src={content.image.url}
+          width={content.image.width}
+          height={content.image.height}
+          alt={content.description}
+          className=""></img>
+      )}
+      <div>
+        <h2 className="text-4xl text-center">{content.header}</h2>
+        <div className="text-xl sm:text-2xl leading-10 tracking-wide font-libre-baskerville">
+          {documentToReactComponents(content.document)}
+        </div>
       </div>
-      {content.image && <img src={content.image.url} width={content.image.width} height={content.image.height} alt={content.description}></img>}
     </section>
   </BoxShadowWrapper>
 }
